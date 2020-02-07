8fdde682f2b836cbe2595857b96799cb240942f6
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-02-07 15:34:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,65 +3,18 @@ $json = file_get_contents('https://jsonactivity1.herokuapp.com/json_1.php');
 
 $data = json_decode($json,true);
 $list = $data['movies'];
-
-
-
-//var_dump($data);
-//echo "<pre>";
-
-//print_r($list);
 ?>
-<div class="content">
 <h1>Movies</h1>
-</div>
-<div class="content">
-<table border="1px">
-    <tr class="head">
-        <td>Title</td>
-        <td>Actor</td>
-        <td>Genre</td>
-        <td>Director</td>
-    </tr>
 <?php
 foreach($list as $value){
     ?>
-    <tr>
+    <ul>
         <td><?php echo $value['mov_title'];?></td>
         <td><?php echo $value['mov_actor'];?></td>
         <td><?php echo $value['mov_genre'];?></td>
         <td><?php echo $value['mov_director'];?></td>
-    </tr>
- 
+    </ul>
 <?php
 }
 ?>
-</table>
-
-<style>
-
-    
-    }
-	table{
-		border: 1px solid black;
-		width: 90%;
-		text-align: center;
-		
-		
-	}
-	th, td{
-		border: 1px solid #ddd;
-		padding: 10px;
-		
-
-	}
-    tr:nth-child(even) {background-color: white;}
-	a {
-		color: #66fcf1;
-	}
-    .head{
-		background-color: grey;
-		color: orange;
-		font-weight: bold;
-	}
-</style>
 </div>
