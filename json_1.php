5c58960ec300e1f3d2902224f047dc1299fd48bc
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-02-07 14:15:24 +0800

    Update json_1.php

--- json_1.php
+++ json_1.php
@@ -2,9 +2,9 @@
 include 'config.php';
 include 'class.movie.php';
 header('Content-Type: application/json');
-$movie = new movie();
-$list=$movie->get_movie();
-echo "{\"movie\":";
+$movies = new movies();
+$list=$movies->get_movies();
+echo "{\"movies\":";
 echo json_encode($list);
 echo "}";
 ?>
