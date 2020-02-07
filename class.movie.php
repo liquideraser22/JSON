0f3d53fa92fc47c816c9ad19926d755721328ac0
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-02-07 13:08:32 +0800

    Create class.movie.php

---
+++ class.movie.php
@@ -0,0 +1,24 @@
+<?php
+class Movies{
+	public $db;
+	
+	public function __construct(){
+		$this->db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}
+	}
+	
+	public function get_movies(){
+		$sql = "SELECT * FROM tbl_movies";
+		$result = mysqli_query($this->db,$sql);
+		while($row = mysqli_fetch_assoc($result)){
+			$list[] = $row;
+		}
+		return $list;
+	}
+	
+}
+?>
+
